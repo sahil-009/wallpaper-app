52a19ad13dca8fb22665d77863b8036153c48499
sahil-009 <sahilmund.6685@gmail.com>
2025-07-29 22:57:50 +0530

    added routes

--- app/(tabs)/account.tsx
+++ app/(tabs)/account.tsx
@@ -1,9 +1,10 @@
-import { Text ,View } from 'react-native';
+import {  Text ,View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-export default function explore() {
+
+export default function account() {
   return (
     <SafeAreaView>
-      <Text>Explore</Text>
+      <Text>Account</Text>
     </SafeAreaView>
   );
 }
